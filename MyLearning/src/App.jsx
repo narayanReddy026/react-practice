94b2198e8b707c14e0f988ec99e4c3fef2ccb80c
Narayan Reddy <baddam.narayan@gmail.com>
2024-07-26 00:46:19 -0400

    created new project

--- MyLearning/src/App.jsx
+++ MyLearning/src/App.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("olive");
   return (
     <>
-      <h1 className=" text-5xl p-6 bg-red-500">I am Perfect</h1>
+      <div
+        className="  w-full h-screen duration-200  "
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 gap-12">
+          <button
+            onClick={() => setColor("red")}
+            className="rounded-full px-4 py-1 text-white bg-red-600 shadow-lg"
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="rounded-full px-4 py-1 text-white bg-green-600 shadow-lg"
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="rounded-full px-4 py-1 text-white bg-blue-600 shadow-lg"
+          >
+            Blue
+          </button>
+        </div>
+      </div>
     </>
   );
 }
