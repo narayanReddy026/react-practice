b9d4cd1e099901abad2c790637bcbe1cb80916cc
Narayan Reddy <baddam.narayan@gmail.com>
2024-07-25 23:40:21 -0400

    changes

--- MyLearning/src/App.jsx
+++ MyLearning/src/App.jsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
-
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  //let counter = 15;
-  const addValue = () => {
-    setCount(count + 1);
-  };
   return (
     <>
-      <h1>I am perfecting react</h1>
-      <h2>Counter value : {count}</h2>
-      <button onClick={addValue}>Add value</button>
-      <button>remove value</button>
-      <p>footer: {count}</p>
+      <h1 className=" text-5xl p-6 bg-red-500">I am Perfect</h1>
     </>
   );
 }
